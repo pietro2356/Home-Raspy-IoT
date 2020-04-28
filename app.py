24a77d32416966d2a8cb38c3e446856ca19f966d
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-04-28 11:05:47 +0200

    Inizializzato il server flask.

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask
+from gpiozero import LED
+
+radioScanner = LED(14)  # Only on
+radioCRI = LED(23)
+radioVVF = LED(25)
+luce = LED(15)
+computer = LED(18)  # Only ON
+homeMini = LED(7)  # Only ON
+cell = LED(8)
+svegliaVar = LED(1)  # Only ON
+
 
 app = Flask(__name__)
 
