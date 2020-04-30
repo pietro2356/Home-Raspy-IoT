8b85587a1a58e6f1348e0baa0e361dd0ce263582
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2020-05-01 00:45:45 +0200

    Fine prima implementazione Telegram

--- app.py
+++ app.py
@@ -15,12 +15,45 @@ svegliaVar = LED(1)  # Only ON
 
 
 # ---------------------------------
+# TELEGRAM FUNCTION
 def botToken():
     return "<<Telegram bot token>>"     # Lo fornisce BotFather alla creazione del bot telegram.
 
 
 def myId():
     return -1   # ChatID del proprietario.
+
+# ~~UNICODE PER EMOJI~~
+def greenSquare():
+    return u'\U00002705'
+
+
+def redSquare():
+    return u'\U0000274C'
+
+
+def casaEM():
+    return u'\U0001F3E0'
+
+
+def radioEM():
+    return u'\U0001F4FB'
+
+
+def faceEM():
+    return u'\U0001F917'
+
+
+def teschioEM():
+    return u'\U00002620'
+
+
+def boomEM():
+    return u'\U0001F4A5'
+
+
+def goEM():
+    return u'\U00002733'
 # ---------------------------------
 
 
@@ -189,6 +222,108 @@ def sveglia(val):
         return "OK", 200
     else:
         return "Error", 500
+
+
+keyboard = ReplyKeyboardMarkup(keyboard=[
+    ['/START', '/STATUS'],
+    ['RADIO ON' + radioEM() + greenSquare(), 'RADIO OFF' + radioEM() + redSquare()],
+    ['CASA ON' + casaEM() + greenSquare(), 'CASA OFF' + casaEM() + redSquare()],
+    ['ALL ON' + greenSquare(), 'ALL OFF' + redSquare()],
+    ['/ADV']])
+
+
+def isOnline():
+    bot.sendMessage(myId(), "Bot online", reply_markup=keyboard)
+
+
+def handle(msg):
+    contentType, chatType, chatId = telepot.glance(msg)
+    text = msg['text'].upper()
+
+    if not (chatId == myId()):
+        bot.sendMessage(chatId, "Non sei autorizzato ad utilizzare questo bot!" + teschioEM())
+
+    # RADIO
+    elif text == "RADIO ON" + radioEM() + greenSquare():
+        radio("1")
+        bot.sendMessage(myId(), "Radio accese" + greenSquare())
+    elif text == "RADIO OFF" + radioEM() + redSquare():
+        radio("0")
+        bot.sendMessage(myId(), "Radio spente" + redSquare())
+
+    # CASA
+    elif text == "CASA ON" + casaEM() + greenSquare():
+        casa("1")
+        bot.sendMessage(myId(), "Casa accesa" + greenSquare())
+    elif text == "CASA OFF" + casaEM() + redSquare():
+        casa("0")
+        bot.sendMessage(myId(), "Casa spenta" + redSquare())
+
+    # TUTTO
+    elif text == "ALL ON" + greenSquare():
+        all("1")
+        bot.sendMessage(myId(), "Tutto acceso" + greenSquare())
+    elif text == "ALL OFF" + redSquare():
+        all("0")
+        bot.sendMessage(myId(), "Tutto spento" + redSquare())
+
+    # START
+    elif text == '/START':
+        bot.sendMessage(myId(), "Benvenuto!" + faceEM(), reply_markup=keyboard)
+
+    # BACK
+    elif text == '/BACK':
+        bot.sendMessage(myId(), 'Sessione avanzata disabilitata.', reply_markup=keyboard)
+
+    # COMANDI AVANZATI
+    elif text == 'RADIO SCANNER ON':
+        radSc("1")
+        bot.sendMessage(myId(), "Radio scanner acceso" + greenSquare())
+    elif text == 'RADIO SCANNER OFF':
+        radSc("0")
+        bot.sendMessage(myId(), "Radio scanner spento" + redSquare())
+
+    elif text == '118 ON':
+        cri("1")
+        bot.sendMessage(myId(), "Radio 118 accesa" + greenSquare())
+    elif text == '118 OFF':
+        cri("0")
+        bot.sendMessage(myId(), "Radio 118 spenta" + redSquare())
+
+    elif text == 'VVF ON':
+        vvf("1")
+        bot.sendMessage(myId(), "Radio VVF accesa" + greenSquare())
+    elif text == 'VVF OFF':
+        vvf("0")
+        bot.sendMessage(myId(), "Radio VVF spenta" + redSquare())
+
+    elif text == 'LUCE ON':
+        light("1")
+        bot.sendMessage(myId(), "Luce accesa" + greenSquare())
+    elif text == 'LUCE OFF':
+        light("0")
+        bot.sendMessage(myId(), "Luce spenta" + redSquare())
+
+    elif text == 'COMPUTER ON':
+        pc("1")
+        bot.sendMessage(myId(), "Computer acceso" + greenSquare())
+    elif text == 'COMPUTER OFF':
+        pc("0")
+        bot.sendMessage(myId(), "Computer spento" + redSquare())
+
+    elif text == 'CARICATORE ON':
+        charger("1")
+        bot.sendMessage(myId(), "Carica cell acceso" + greenSquare())
+    elif text == 'CARICATORE OFF':
+        charger("0")
+        bot.sendMessage(myId(), "Carica cell spento" + redSquare())
+
+    else:
+        bot.sendMessage(myId(), "Comando non valido!" + redSquare() + redSquare())
+
+
+isOnline()
+MessageLoop(bot, handle).run_as_thread()
 
 
 app.run(host='0.0.0.0')
